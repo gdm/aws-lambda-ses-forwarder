e6f9d856f4b0b0a363d94e22f0b52f9635ccda01
Dmytro Gorbunov <dmitro.gorbunov@gmail.com>
2019-09-30 02:15:16 +0200

    Setup aws cli v2, some issues with s3 bucket permission when setup ses rule

---
+++ run.sh
@@ -0,0 +1,5 @@
+
+aws cloudformation create-stack --stack-name SES --template-body file://ses.yml  --parameters ParameterKey=ConfigSetName,ParameterValue=ManagedConfigSetName ParameterKey=DomainRuleSetName,ParameterValue=ManagedDomainRuleSetName ParameterKey=DomainName,ParameterValue=contrall.info
+
+error: 
+Could not write to bucket: ses-emailss3bucket-1p46c11suzw9p (Service: AmazonSimpleEmailService; Status Code: 400; Error Code: InvalidS3Configuration; Request ID: c36f1c6d-fe17-449a-9035-3eac54391d10)
