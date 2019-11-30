c37aac4c76227c5c3f534df66914e0d2e54f2faf
Dmytro Gorbunov <dmitro.gorbunov@gmail.com>
2019-11-30 23:15:23 +0100

    Autodeployment lambda with autoincremented version

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,20 @@
 #!/bin/bash -xe
 
+CURRENT_VERSION=$(grep -o -P '(?<=lambda-forwarder\/v)\d+' ses.yml)
+
+NEW_VERSION=$((CURRENT_VERSION + 1))
+
+NEW_VERSION_ALIGNED=$(printf "%04d" "$NEW_VERSION")
+
 rm -r function.zip
 cd s3-lambda
 zip -r ../function.zip .
 cd -
-aws s3 cp function.zip s3://info-contrall-code/lambda-forwarder/v1/function.zip
+
+
+aws s3 cp function.zip s3://info-contrall-code/lambda-forwarder/v${NEW_VERSION_ALIGNED}/function.zip
+
+sed -i -e  "s|lambda-forwarder/v.*|lambda-forwarder/v${NEW_VERSION_ALIGNED}/function.zip|" ses.yml
 
 AWS_PROFILE=dmgo-admin aws cloudformation deploy --stack-name SES --template-file ses.yml --capabilities CAPABILITY_IAM
 
--- s3-lambda/index.js
+++ s3-lambda/index.js
@@ -2,5 +2,5 @@
 var aws = require('aws-sdk');
 
 exports.handler = function(event, context) {
-  console.log("Request received (code from s3 bucket):\n", JSON.stringify(event));
+  console.log("Request received (code from s3 bucket v2):\n", JSON.stringify(event));
 };
