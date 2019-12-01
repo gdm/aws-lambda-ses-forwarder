0324ce23d9aa5f8e1d9b641e6adde9b0aff9a2a1
Dmytro Gorbunov <dmitro.gorbunov@gmail.com>
2019-12-01 13:48:29 +0100

    First working version with hardcoded bucket name

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -xe
 
+npm test
+
 CURRENT_VERSION=$(grep -o -P '(?<=lambda-forwarder\/v)\d+' ses.yml)
 
 NEW_VERSION=$((CURRENT_VERSION + 1))
@@ -7,10 +9,13 @@ NEW_VERSION=$((CURRENT_VERSION + 1))
 NEW_VERSION_ALIGNED=$(printf "%04d" "$NEW_VERSION")
 
 rm -r function.zip
+cp index.js s3-lambda/aws-lambda-ses-forwarder.js
 cd s3-lambda
+
 zip -r ../function.zip .
 cd -
 
+rm s3-lambda/aws-lambda-ses-forwarder.js
 
 aws s3 cp function.zip s3://info-contrall-code/lambda-forwarder/v${NEW_VERSION_ALIGNED}/function.zip
 
--- s3-lambda/index.js
+++ s3-lambda/index.js
@@ -1,6 +1,28 @@
 
 var aws = require('aws-sdk');
 
-exports.handler = function(event, context) {
-  console.log("Request received (code from s3 bucket v2):\n", JSON.stringify(event));
+var LambdaForwarder = require("./aws-lambda-ses-forwarder.js");
+
+exports.handler = function(event, context, callback) {
+  console.log("Request received:\n", JSON.stringify(event));
+  // See aws-lambda-ses-forwarder/index.js for all options.
+  var overrides = {
+    config: {
+      fromEmail: "noreply@contrall.info",
+      emailBucket: "ses-emailss3bucket-6qhwieg4kbqy",
+      emailKeyPrefix: "forwarder/",
+      forwardMapping: {
+        "test123@contrall.info": [
+          "dmytro.gorbunov@gmail.com"
+        ],
+        "test124@contrall.info": [
+          "dmitro.gorbunov@gmail.com"
+        ],
+        "@contrall.info": [
+          "dmytro.gorbunov@gmail.com"
+        ]
+      }
+    }
+  };
+  LambdaForwarder.handler(event, context, callback, overrides);
 };
